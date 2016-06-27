d511457cec76f80349ed07565a4357a3e0f340ee
Nick Spain <nicholas.spain96@gmail.com>
2016-06-27 23:15:15 +1000

    Remove cask

--- .cask/24.5.1/elpa/ansi-20150703.126/ansi-pkg.el
+++
@@ -1,1 +0,0 @@
-(define-package "ansi" "20150703.126" "Turn string into ansi strings" '((s "1.6.1") (dash "1.5.0")) :url "http://github.com/rejeep/ansi" :keywords '("color" "ansi"))
--- .cask/24.5.1/elpa/espuds-20151114.759/espuds-pkg.el
+++
@@ -1,1 +0,0 @@
-(define-package "espuds" "20151114.759" "Ecukes step definitions" '((s "1.7.0") (dash "2.2.0") (f "0.12.1")) :url "http://github.com/ecukes/espuds" :keywords '("test"))
